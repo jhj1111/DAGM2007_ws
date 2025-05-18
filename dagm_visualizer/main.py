527a705f5e9f90b90a94166cc8a362825c4a111c
jhj1111 <happyijun@naver.com>
2025-05-18 11:27:37 +0900

    add 파일 실행 옵션 추가

--- dagm_visualizer/main.py
+++ dagm_visualizer/main.py
@@ -20,6 +20,19 @@ if __name__ == "__main__":
     vis = Visualizer(resize_shape=(512, 512))
     ctrl = Controller(dm, vis)
 
+    def select_params(class_idx, idx, defect, random_select):
+        if class_idx and idx:
+            if defect is None:
+                ctrl.show_image(class_idx=class_idx, idx=idx)
+            else:
+                ctrl.show_image(class_idx=class_idx, idx=idx, defect=defect)
+        else:
+            random_select = True
+            if defect or defect is None:
+                ctrl.show_image(defect=defect, random_select=random_select)
+            else:
+                ctrl.show_image(random_select=random_select)
+        
     # CLI 인자 파싱
     parser = argparse.ArgumentParser(description="DAGM2007 이미지 시각화 도구")
     parser.add_argument('--class', dest='class_id', type=int, help='클래스 번호 (1~5)')
@@ -29,13 +42,19 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    class_idx = None
+    idx = None
+    defect = True
+    random_select = None
+
     # 실행
-    if args.random :
-        ctrl.show_image(defect=args.defect, random_select=True)
-    elif args.class_id is None and args.idx is None:
-        ctrl.show_image(defect=True, random_select=True)
+    if not (args.class_id and args.idx and args.defect and args.random):    # 파일 실행
+        select_params(class_idx, idx, defect, random_select)
     else:
-        if args.class_id is None or args.idx is None:
-            print("랜덤 모드가 아니면 --class와 --idx를 모두 지정해야 합니다.")
+        if args.random :
+            ctrl.show_image(defect=args.defect, random_select=True)
         else:
-            ctrl.show_image(class_idx=args.class_id, idx=args.idx, defect=args.defect)
+            if args.class_id is None or args.idx is None:
+                print("랜덤 모드가 아니면 --class와 --idx를 모두 지정해야 합니다.")
+            else:
+                ctrl.show_image(class_idx=args.class_id, idx=args.idx, defect=args.defect)
